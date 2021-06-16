d25c49e4a00c5e8c8409e41d8c54a1141759912e
I Wayan Rama Swara <74579764+RamaSwara042@users.noreply.github.com>
2021-06-16 16:00:37 +0800

    Add files via upload

---
+++ Bilik Pernik/src/Pencarian/ProsesSearching.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pencarian;
+
+/**
+ *
+ * @author iwaya
+ */
+public class ProsesSearching {
+    
+    private String namaproduk;
+
+    public ProsesSearching(String namaProduct) {
+        this.namaproduk = namaProduct;
+    }
+
+    public String getNamaproduk() {
+        return namaproduk;
+    }
+
+    public void setNamaproduk(String namaproduk) {
+        this.namaproduk = namaproduk;
+    }
+
+}
